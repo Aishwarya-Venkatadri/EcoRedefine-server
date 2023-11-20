b8b08e5fc6b6b420bb69eaffa37e7cb948428430
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-20 14:37:05 -0800

    migrations for categories

--- migrations/20231120221631_create_categories.js
+++ migrations/20231120221631_create_categories.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("categories", (table) => {
+      table.increments("category_id").primary(); ///PK
+      table.string("category_name").notNullable();
+    });
+  };
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+    return knex.schema.dropTable("categories");
+  };
   
-};
