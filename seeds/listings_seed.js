1f0c0800abd69360e7494c56d773dad1b636f5ab
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-20 15:53:38 -0800

    seeding dummy data for listing

--- seeds/listings_seed.js
+++ seeds/listings_seed.js
@@ -4,10 +4,21 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('listings').del()
+  await knex('listings').insert([
+    {
+      listing_id: 1,
+      listing_name: "Learning React: Modern Patterns for Developing React Apps",
+      category_id: 1,
+      image: 'listing_01.jpg',
+      location: 'Burnaby',
+      availability: 'Available',
+      user_id: 1,
+      email: "aishwarya@gmail.com",
+      address: 'Royal Oak Burnaby',
+      listing_weight: "494 grams",
+      listing_material: "Paper",
+      listing_borrow_price: '10.50'
+    }
   ]);
 };
