84e526311f0cce58037b27bbfb4d9ff2896a77c5
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-20 14:38:18 -0800

    adds migration for testimonials

--- migrations/20231120221702_create_testimonials.js
+++ migrations/20231120221702_create_testimonials.js
@@ -2,14 +2,25 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("testimony", (table) => {
+      table.increments("id").primary(); // PK
+      table.integer("user_id").unsigned().notNullable(); // FK referencing users 
+      table.string("testimony").notNullable();
+      table.integer("stars").notNullable();
+      table.integer("listing_id").unsigned().notNullable(); //FK referencing listings 
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+      // Foreign key constraints
+      table.foreign("user_id").references("user_id").inTable("users");
+      table.foreign("listing_id").references("listing_id").inTable("listings");
+    });
+  };
   
-};
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+    return knex.schema.dropTable("testimony");
+  };
+  
