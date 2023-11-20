d64ee6ac55cb2c55c1155fce3e117e013042b6c3
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-20 14:25:39 -0800

    adds the migration/ table for listings

--- migrations/20231120221607_create_listings.js
+++ migrations/20231120221607_create_listings.js
@@ -2,14 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+exports.up = function (knex) {
+    return knex.schema.createTable("listings", (table) => {
+      table.increments("listing_id").primary(); //PK
+      table.string("listing_name").notNullable();
+      table.integer("category_id").unsigned().notNullable(); // FK referencing categories
+      table.string("image").notNullable();
+      table.string("location").notNullable();
+      table.boolean("availability").notNullable();
+      table.integer("user_id").unsigned().notNullable(); // FK referencing users 
+      table.string("email").notNullable();
+      table.string("address").notNullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.string("listing_weight").notNullable();
+      table.string("listing_material").notNullable();
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+      // Foreign key constraints
+      table.foreign("category_id").references("category_id").inTable("categories");
+      table.foreign("user_id").references("user_id").inTable("users");
+    });
+  };
   
-};
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function (knex) {
+    return knex.schema.dropTable("listings");
+  };
+  
