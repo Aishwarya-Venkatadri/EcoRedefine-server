d77a85e9fed6eead8be15c806b47c358c12518a3
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-20 16:10:51 -0800

    ignores node_modules and .env files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 5050;
+require('dotenv').config();
+
+//MiddleWare
+app.use(express.json()); 
+app.use(cors());
+app.use(express.static('public')); //static files -- images served public
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello, Welcome to Home Route")
+});
+
+app.listen(PORT, ()=>{
+    console.log(`server running on ${PORT}`)
+});
