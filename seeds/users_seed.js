305170b1d8257556e9f3e461acdeccc099ec35c6
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-20 20:54:37 -0800

    adds users seed

--- seeds/users_seed.js
+++ seeds/users_seed.js
@@ -1,13 +1,20 @@
+// users_seed.js
+
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('users').del();
+
+  // Seed a single user
+  await knex('users').insert([
+    {
+      user_id: 1,
+      user_name: "capstonetest",
+      password: "testtest123" //  password is not hashed, MVP
+    }
+   
   ]);
 };
