69ec2f97d5b39475b1919f657eec6c7f84deb2ed
Aishwarya <aishwaryabhargav@outlook.com>
2023-11-23 18:29:28 -0800

    adds authentication logic 🛂

---
+++ Middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+// authMiddleware.js
+
+const jwt = require('jsonwebtoken');
+
+const authenticateUser = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  // Verify the token
+  jwt.verify(token, 'your-secret-key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    // Attach user information to the request object
+    req.user = { userId: decoded.userId };
+    next();
+  });
+};
+
+module.exports = { authenticateUser };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const categoryRoutes = require('./routes/categories-routes');
 const listingRoutes = require('./routes/listings-routes.js');
 const testimonialsRoutes = require('./routes/testimonials-routes.js')
+const authRoutes = require('./routes/authRoutes.js')
 const dotenv = require('dotenv');
 
 const app = express();
@@ -15,12 +16,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public')); // static files -- images served public
 
+
 // Routes
 app.use("/categories", categoryRoutes);
 app.use("/listings", listingRoutes);
 app.use('/testimonials', testimonialsRoutes);
+app.use('/login', authRoutes);
 
-// Default route
+
+// Home route
 app.get('/', (req, res) => {
     res.send("Hello, Welcome to Home Route");
 });
